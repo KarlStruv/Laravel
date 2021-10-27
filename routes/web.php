5365c0bdd3dd82d240fa68442e7458bf18945959
Kārlis Strūve <struve3@gmail.com>
2021-10-28 01:50:45 +0300

    Added Routing and Models
    Added routing for tasks(create, edit, delete) and users(login, register).
    Added models for User object and Task object.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,8 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 //Route::get('/tasks', [TasksController::class, 'index']);
 
-Route::get('/tasks', function(){ return view('index'); });
-Route::get('/tasks/create', function(){ return view('create'); });
-Route::get('/', function(){ return view('login'); });
-Route::get('/register', function(){ return view('register'); });
+Route::get('/', [TasksController::class, 'showLoginForm']);
+Route::post('/', [TasksController::class, 'login']);
 
+Route::get('/register', [TasksController::class, 'showRegisterFrom']);
+Route::post('/register', [TasksController::class, 'register']);
+
+Route::get('/tasks', [TasksController::class, 'index']);
+Route::prefix('/task')->group(function (){
+    Route::post('/create', [TasksController::class, 'create']);
+    Route::put('/{id}', [TasksController::class, 'edit']);
+    Route::delete('/{id}', [TasksController::class, 'delete']);
+});
